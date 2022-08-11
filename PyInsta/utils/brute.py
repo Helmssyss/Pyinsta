2e450401eb04f9b735ffaff372c31c71db3ec4f2
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2022-08-11 16:25:58 +0300

    Add files via upload

--- PyInsta/utils/brute.py
+++ PyInsta/utils/brute.py
@@ -148,9 +148,9 @@ class Bruter:
                 print(f"{Fore.MAGENTA}[ {Fore.RED}!{Fore.MAGENTA} ] Password not found.{Fore.RESET}")
             
             else:
-                print(f"{Fore.MAGENTA}[ {Fore.RED}+{Fore.MAGENTA} ] Password is Found : {Console.CYAN}{self.__passw}{Fore.RESET}")
+                print(f"{Fore.MAGENTA}[ {Fore.RED}+{Fore.MAGENTA} ] Password is Found : {Fore.CYAN}{self.__passw}{Fore.RESET}")
 
-            print(f"{Fore.MAGENTA}[ {Fore.RED}-{Fore.MAGENTA} ] Brute Force Attack is completed\n[ {Console.CYAN}?{Fore.MAGENTA} ]\n  ╰────≻ Attack Lasted {str(now_time)+' Seconds.' if now_time < 60 else str((now_time/60).__round__(3))+' Minutes.'}{Fore.RESET}")
+            print(f"{Fore.MAGENTA}[ {Fore.RED}-{Fore.MAGENTA} ] Brute Force Attack is completed\n[ {Fore.CYAN}?{Fore.MAGENTA} ]\n  ╰────≻ Attack Lasted {str(now_time)+' Seconds.' if now_time < 60 else str((now_time/60).__round__(3))+' Minutes.'}{Fore.RESET}")
         
         except Exception as e:
             print(e)
