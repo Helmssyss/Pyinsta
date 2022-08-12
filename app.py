fc919136e4dba1425ac6eee3a4f4e087ee0bb384
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2022-08-12 15:00:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from configparser import ConfigParser
 from platform import python_version
 
 init(autoreset=True) # Colorama
-print(version_control.ctrl.readLocalVersion)
 class App(Instagram):
     def __init__(self, username: str = ..., password: str = ...) -> None:
         self.__userN = username
@@ -24,6 +23,7 @@ class App(Instagram):
 
         else:
             os.system("cls")
+            print(version_control.ctrl.readLocalVersion)
             print(f"{Fore.CYAN}{self.__userN}>{Console.BANNER}")
             self.main()
 
